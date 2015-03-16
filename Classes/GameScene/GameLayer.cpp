034040a15f2f9b5e27ecc35d22fe1a8d091ebec0
KeyboardFire <andy@keyboardfire.com>
2015-03-15 20:04:06 -0500

    actually fixed the bug

--- Classes/GameScene/GameLayer.cpp
+++ Classes/GameScene/GameLayer.cpp
@@ -50,8 +50,8 @@ bool GameLayer::init() {
         bool retry;
         do {
             slime->setPosition(Vec2(
-                RandomHelper::random_int(1, (int) map->getContentSize().width-1),
-                RandomHelper::random_int(1, (int) map->getContentSize().height-1)
+                RandomHelper::random_int((int) map->getTileSize().width, (int) (map->getContentSize().width - map->getTileSize().width)),
+                RandomHelper::random_int((int) map->getTileSize().height, (int) (map->getContentSize().height - map->getTileSize().height))
             ));
 
             retry = false;
